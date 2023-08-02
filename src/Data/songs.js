692656f40417e048371d5b1bdf8219678b8537e8
Glenaldin Halim <glenaldinhalim16@gmail.com>
2023-08-02 20:02:20 +0700

    add dummy data

---
+++ src/Data/songs.js
@@ -0,0 +1,117 @@
+const mostPlayed = [
+    {
+        id: "d739fab5-a56f-4b07-b9b4-07b3629c9a84",
+        title: "One more time, One more chance",
+        artist: "Masayoshi Yamazaki",
+        album_url: "https://i.scdn.co/image/ab67616d0000485143c9eb81ee7284630b8dedb1"
+    },
+    {
+        id: "35aa9087-8a3d-4b98-b3ce-84312a207b8a",
+        title: "mine",
+        artist: "MY FIRST STORY",
+        album_url: "https://i.scdn.co/image/ab67616d00004851732db795d022e2266c4d9192"
+    },
+    {
+        id: "89f6e4e3-f41d-4ecb-a1ca-0749a418b111",
+        title: "Dreaming of you",
+        artist: "MY FIRST STORY",
+        album_url: "https://i.scdn.co/image/ab67616d00004851758fece77176c137f4e56062"
+    },
+    {
+        id: "b5b553d2-f38d-4aa3-81e6-627d4f246075",
+        title: "BLACK MEMORY",
+        artist: "THE ORAL CIGARETTES",
+        album_url: "https://i.scdn.co/image/ab67616d000048519bf12be6cffdb867777285e3"
+    },
+    {
+        id: "87c5f0fb-069d-4ed8-ba7d-508dcfba3af1",
+        title: "最後の花弁 (The meaning of love)",
+        artist: "EGOIST",
+        album_url: "https://i.scdn.co/image/ab67616d00004851c5a57ca4a55f483a8a182726"
+    },
+]
+
+const dailyMix = [
+    {
+        id: "9cdf88b9-6cf8-4396-bb71-7c621080a85b",
+        title: "The Beginning",
+        artist: "ONE OK ROCK",
+        album: "Jinsei Kakete Boku Wa",
+        album_url: "https://i.scdn.co/image/ab67616d00001e02cff66b3375bbeb03c6c214a0",
+        duration: "4:52"
+    },
+    {
+        id: "799c1a13-7818-4e36-8090-533bf780972b",
+        title: "Renegades",
+        artist: "ONE OK ROCK",
+        album: "Renegades",
+        album_url: "https://i.scdn.co/image/ab67616d0000485145b5aedf82fd7fa05ef5ad11",
+        duration: "4:04"
+    },
+    {
+        id: "3ce9fb8a-1e0f-464c-9069-aad4a9f1fa0a",
+        title: "Broken Heart of Gold",
+        artist: "ONE OK ROCK",
+        album: "Broken Heart of Gold",
+        album_url: "https://i.scdn.co/image/ab67616d000048518ca492895c21886b02107381",
+        duration: "4:13"
+    },
+    {
+        id: "e14c518d-3a49-441d-a768-61d6a81b4279",
+        title: "アンダーグラウンド",
+        artist: "MY FIRST STORY",
+        album: "V",
+        album_url: "https://i.scdn.co/image/ab67616d00004851adcee9ce71494b1f80e1245b",
+        duration: "4:29"
+    },
+    {
+        id: "80c5e6c3-b9ff-4477-8056-571496285982",
+        title: "Deep Down",
+        artist: "Aimer",
+        album: "Deep Down",
+        album_url: "https://i.scdn.co/image/ab67616d0000485167b3ddc2f162440b8953c5c9",
+        duration: "3:47"
+    },
+    {
+        id: "8f637bb6-573f-4252-a3f5-f8d0fc4bc474",
+        title: "KICK BACK",
+        artist: "Kenshi Yonezu",
+        album: "KICK BACK",
+        album_url: "https://i.scdn.co/image/ab67616d00004851303d8545fce8302841c39859",
+        duration: "4:11"
+    },
+    {
+        id: "9498588b-61a4-46a9-8253-576c6a50c48c",
+        title: "DAYBREAK'S BELL",
+        artist: "L'Arc-en-Ciel",
+        album: "KISS",
+        album_url: "https://i.scdn.co/image/ab67616d0000485139f8f6d1b6961dc3137742e9",
+        duration: "4:11"
+    },
+    {
+        id: "27a7f819-d3d2-40b0-a7b6-ae2287f46afa",
+        title: "SOUVENIR",
+        artist: "BUMP OF CHICKEN",
+        album: "SOUVENIR",
+        album_url: "https://i.scdn.co/image/ab67616d000048514215e4b973011670aaad1d9b",
+        duration: "4:24"
+    },
+    {
+        id: "0be5c50c-f59d-4762-b93e-58405fa18460",
+        title: "Red Criminal",
+        artist: "THE ORAL CIGARETTES",
+        album: "Red Criminal",
+        album_url: "https://i.scdn.co/image/ab67616d00004851f37120383b818c1e4a930410",
+        duration: "4:24"
+    },
+    {
+        id: "b6638523-30e6-4886-807c-4af449bcff32",
+        title: "MACHINEGUN",
+        artist: "THE ORAL CIGARETTES",
+        album: "MACHINEGUN",
+        album_url: "https://i.scdn.co/image/ab67616d000048511a70f6e36f7dbb20891aca0f",
+        duration: "3:41"
+    },
+]
+
+export { mostPlayed, dailyMix}
